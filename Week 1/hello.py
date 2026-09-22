2ec0684f16af3a98f944739c8b9a68a4fca75134
Eugene <2603188@sit.singaporetech.edu.sg>
2026-09-22 14:46:52 +0800

    Activity 3

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -4,3 +4,21 @@ print("Welcome here")
 print("My first post!")
 print("======================")
 
+#Activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Activity 3
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
